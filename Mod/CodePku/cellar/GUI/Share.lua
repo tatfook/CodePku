80670840e9a873458dc1c949f857099a5eca9141
qiujl <13387110695@163.com>
2020-07-16 11:15:53 +0800

    Adaptive mobile

--- Mod/CodePku/cellar/GUI/Share.lua
+++ Mod/CodePku/cellar/GUI/Share.lua
@@ -19,30 +19,12 @@ function SharePage.OnClickBlock(block_id)
 end
 
 function SharePage:ShowPage(bShow)
-    NPL.load("(gl)script/apps/Aries/Creator/Game/Areas/DesktopMenuPage.lua");
-    local DesktopMenuPage = commonlib.gettable("MyCompany.Aries.Creator.Game.Desktop.DesktopMenuPage");
+    -- NPL.load("(gl)script/apps/Aries/Creator/Game/Areas/DesktopMenuPage.lua");
+    -- local DesktopMenuPage = commonlib.gettable("MyCompany.Aries.Creator.Game.Desktop.DesktopMenuPage");
     SharePage.bForceHide = bShow == false;
-    local params = {
-        url = "Mod/CodePku/cellar/GUI/Share.html", 
-        name = "SharePage.ShowPage", 
-        isShowTitleBar = false,
-        DestroyOnClose = true,
-        bToggleShowHide=true, 
-        style = CommonCtrl.WindowFrame.ContainerStyle,
-        allowDrag = false,
-        enable_esc_key = true,
-        bShow = bShow,
-        click_through = false, 
-        zorder = 20,
-        app_key = MyCompany.Aries.Creator.Game.Desktop.App.app_key, 
-        directPosition = true,
-        align = "_ct",
-        x = -1920/2,
-        y = -1080/2,
-        width = 1920,
-        height = 1080,
-        };
-    System.App.Commands.Call("File.MCMLWindowFrame", params);
-
-    -- ParaUI.SetMinimumScreenSize(1920,1080,true);
+    NPL.load("(gl)Mod/CodePku/cellar/GUI/Window/AdaptWindow.lua");
+    local AdaptWindow = commonlib.gettable("Mod.CodePku.GUI.Window.AdaptWindow")
+    AdaptWindow:QuickWindow({url = "Mod/CodePku/cellar/GUI/Share.html", 
+    alignment="_ct", left = -960, top = -540, width = 1920, height = 1080,zorder =30})
 end
+        
