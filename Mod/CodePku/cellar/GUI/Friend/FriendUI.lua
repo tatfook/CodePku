4fa9c6505ef47249f28430329aed626154acfe2b
Zz-feng <13517281607@163.com>
2020-07-23 10:14:40 +0800

    add and delete

--- Mod/CodePku/cellar/GUI/Friend/FriendUI.lua
+++ Mod/CodePku/cellar/GUI/Friend/FriendUI.lua
@@ -31,6 +31,14 @@ FriendUI.popparams={
         url="Mod/CodePku/cellar/GUI/Friend/popup/search.html",
         alignment="_ct", left = -960, top = -540, width = 1920, height = 1080,zorder =30
     },
+    add = {
+        url="Mod/CodePku/cellar/GUI/Friend/popup/add.html",
+        alignment="_ct", left = -960, top = -540, width = 1920, height = 1080,zorder =30
+    },
+    delete = {
+        url="Mod/CodePku/cellar/GUI/Friend/popup/delete.html",
+        alignment="_ct", left = -960, top = -540, width = 1920, height = 1080,zorder =30
+    }
 }
 
 FriendUI.vars = {}
@@ -64,5 +72,11 @@ function FriendUI:ShowPopup(PopIndex)
 
     if PopIndex == 1 then
         FriendUI.popui = AdaptWindow:QuickWindow(FriendUI.popparams["search"])
+    end
+    if PopIndex == 2 then
+        FriendUI.popui = AdaptWindow:QuickWindow(FriendUI.popparams["add"])
+    end
+    if PopIndex == 4 then
+        FriendUI.popui = AdaptWindow:QuickWindow(FriendUI.popparams["delete"])
     end
 end
