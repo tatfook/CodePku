9a651dc04e7799aa233fce7562fac7fadbc22cd0
penglu <635929108@qq.com>
2020-07-31 09:04:37 +0800

    Revert "排行榜----将显示经验值修改为当前等级"
    This reverts commit e9ee9f620b20adad05447cc27abc1f31a8777ea5.

--- Mod/CodePku/main.lua
+++ Mod/CodePku/main.lua
@@ -57,9 +57,11 @@ local SharePage = NPL.load("(gl)Mod/CodePku/cellar/GUI/Share.lua")
 local FriendUI = NPL.load("(gl)Mod/CodePku/cellar/GUI/Window/AdaptWindow.lua");
 local GenAndNamePage = NPL.load("(gl)Mod/CodePku/cellar/GUI/GenAndName.lua")
 local SubjectPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/Subject.lua")
+local RankPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/RankPage.lua")
 local EditPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/editShare.lua")
+local RankPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/FastEntrence/RankPage.lua")
 local PropsInfo = NPL.load("(gl)Mod/CodePku/cellar/GUI/Props/PropsInfo.lua")
-local RankPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/rank/RankPage.lua")
+local RankPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/FastEntrence/RankPage.lua")
 local Messages = NPL.load("(gl)Mod/CodePku/cellar/common/TouchMiniButtons/Messages.lua")
 
 local DownloadWorld = commonlib.gettable("MyCompany.Aries.Game.MainLogin.DownloadWorld")
