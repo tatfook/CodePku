285409f38c061a2480396b4c1134cdf3e2c706b7
wanglj <wanglj@codepku.com>
2020-08-24 18:15:33 +0800

    2020.8.24新增科学,编程,文史，增加对应的排行榜，专题区，个人中心经验值

--- Mod/CodePku/cellar/GUI/Subject.lua
+++ Mod/CodePku/cellar/GUI/Subject.lua
@@ -17,12 +17,15 @@ end
 function SubjectPage.OnInitSubject()
     SubjectPage.subject_list = {}
     local const_subject = {
-        [1] = {'chinese', '语文', 'codepku/image/textures/profile_32bits.png#1700 164 104 106'},
-        [2] = {'math', '数学', 'codepku/image/textures/profile_32bits.png#1700 281 104 106'},
-        [3] = {'english', '英语', 'codepku/image/textures/profile_32bits.png#1702 398 104 106'},
-        [4] = {'physics', '物理', 'codepku/image/textures/profile_32bits.png#1576 280 104 106'},
-        [5] = {'chemistry', '化学', 'codepku/image/textures/profile_32bits.png#1576 397 104 106'},
-        [6] = {'biology', '生物', 'codepku/image/textures/profile_32bits.png#1579 164 104 106'},
+        [1] = {'chinese', '语文', 'codepku/image/textures/profile_32bits.png#1700 164 104 106',1},
+        [2] = {'math', '数学', 'codepku/image/textures/profile_32bits.png#1700 281 104 106',2},
+        [3] = {'english', '英语', 'codepku/image/textures/profile_32bits.png#1702 398 104 106',3},
+        -- [4] = {'physics', '物理', 'codepku/image/textures/profile_32bits.png#1576 280 104 106'},
+        -- [5] = {'chemistry', '化学', 'codepku/image/textures/profile_32bits.png#1576 397 104 106'},
+        -- [6] = {'biology', '生物', 'codepku/image/textures/profile_32bits.png#1579 164 104 106'},
+        [4] = {'science', '科学', 'codepku/image/textures/profile_32bits.png#1700 164 104 106',13},
+        [5] = {'programming', '编程', 'codepku/image/textures/profile_32bits.png#1700 281 104 106',10},
+        [6] = {'literature_history', '文史', 'codepku/image/textures/profile_32bits.png#1702 398 104 106',12}
     }
     -- SubjectPage.subject_list = {
     --     {name: 'chinese', chname: '语文', icon: 'codepku/image/textures/profile_32bits.png#1700 164 104 106'},
@@ -48,34 +51,34 @@ function SubjectPage.OnInitSubject()
             total_exp = 0,
             name = const_subject[i][1],
             chname = const_subject[i][2],
-            icon = const_subject[i][3]
+            icon = const_subject[i][3],
+            subject_id = const_subject[i][4]
         })                            
     end
 end
 
+function GetSubjectIndex(level_type)
+    -- level_type ==> subject_list Index
+    for k,v in ipairs(SubjectPage.subject_list) do
+        if v['subject_id'] == level_type then
+            return k
+        end
+    end
+    return nil
+end
+
 function SubjectPage.GetLevels()
     SubjectPage.OnInitSubject();
-    local subject_names = {
-        [1] = {'chinese','语文'},
-        [2] = {'math','数学'},
-        [3] = {'english','英语'},
-        [4] = {'physics','物理'},
-        [5] = {'chemistry','化学'},
-        [6] = {'biology','生物'},
-        [7] = {'politics','政治'},
-        [8] = {'history','历史'},
-        [9] = {'geography ','地理'},
-        [10] = {'programming','编程'},
-    }
+
     request:get('/users/levels'):next(function(response)
         if (response.status == 200) then
             local data = response.data.data;
             for i = 1, #data do
-                if data[i].level_type < 7 then
-                    SubjectPage.subject_list[data[i].level_type].level = data[i].current_level .. "级";
-                    SubjectPage.subject_list[data[i].level_type].percent = data[i].current_exp / data[i].next_exp;
-                    SubjectPage.subject_list[data[i].level_type].current_exp = data[i].current_exp;
-                    SubjectPage.subject_list[data[i].level_type].next_exp = data[i].next_exp; -- means total
+                if data[i].level_type and data[i].level_type ~= 11 then
+                    SubjectPage.subject_list[GetSubjectIndex(data[i].level_type)].level = data[i].current_level .. "级";
+                    SubjectPage.subject_list[GetSubjectIndex(data[i].level_type)].percent = data[i].current_exp / data[i].next_exp;
+                    SubjectPage.subject_list[GetSubjectIndex(data[i].level_type)].current_exp = data[i].current_exp;
+                    SubjectPage.subject_list[GetSubjectIndex(data[i].level_type)].next_exp = data[i].next_exp; -- means total
                 end                                       
             end
             SubjectPage:InitWindow();                                           
