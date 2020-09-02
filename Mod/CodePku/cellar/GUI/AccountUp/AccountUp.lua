f59ea512cc0b699721a5a5fc0db9bf72350bb85e
邹韧 <277714787@qq.com>
2020-09-02 11:06:06 +0800

    获取验证码功能完善 最后还有绑定号码功能没做

--- Mod/CodePku/cellar/GUI/AccountUp/AccountUp.lua
+++ Mod/CodePku/cellar/GUI/AccountUp/AccountUp.lua
@@ -10,6 +10,7 @@
 NPL.load("(gl)Mod/CodePku/cellar/GUI/AccountUp/AccountUp.lua")
 local request = NPL.load("(gl)Mod/CodePku/api/BaseRequest.lua");
 local CodePkuServiceSession = NPL.load("(gl)Mod/CodePku/service/CodePkuService/Session.lua")
+local Config = NPL.load("(gl)Mod/CodePku/config/Config.lua")
 
 local AdaptWindow = commonlib.gettable("Mod.CodePku.GUI.Window.AdaptWindow");
 local AccountUp = commonlib.gettable("Mod.CodePku.AccountUp")
@@ -17,14 +18,19 @@ local AccountUp = commonlib.gettable("Mod.CodePku.AccountUp")
 AccountUp.ui = nil
 
 function AccountUp.OnSureBtnCLicked(page)
+    if not AccountUp.mobileToken then
+        GameLogic.AddBBS(nil, L"请先获取验证码", 3000, "255 0 0", 21)
+    end
+    local account = page:GetValue("account")
+    local verifiy_code = page:GetValue("verifiy_code")
     local data = {
-        mobile = "",
-        verifiy_code = "",
-        mobile_token = "",
+        mobile = account,
+        verifiy_code = verifiy_code,
+        mobile_token = AccountUp.mobileToken,
     }
     request:get('/users/mobile', data):next(function(response)
 		if (response.status == 200 and response.data.code == 200) then
-            
+            -- 绑定后的数据处理
         elseif response.data and response.data.message then
             GameLogic.AddBBS("CodeGlobals", response.data.message, 3000, "#00FF00");
         end		
@@ -38,13 +44,14 @@ function AccountUp.GetMobileCode( page )
     end
 
     local account = page:GetValue("account")
+    LOG.std(nil, "AccountUp", "GetMobileCode", "account = %s", tostring(account))
 
     if account and #account ~= 11 then
         GameLogic.AddBBS(nil, L"手机号码位数不对", 3000, "255 0 0", 21)
         return false
     end
 
-    if account or account == "" then
+    if (not account) or account == "" then
         GameLogic.AddBBS(nil, L"手机号码不能为空", 3000, "255 0 0", 21)
         return false
     end
@@ -57,7 +64,6 @@ function AccountUp.GetMobileCode( page )
     local timer = commonlib.Timer:new({
         callbackFunc = function(timer)
             page:SetValue("GetMobileCodeBtn", format("%s(%ds)", L"重新发送", times))
-
             if times == 0 then
                 AccountUp.isClickedGetPhoneCaptcha = false
                 page:SetValue("GetMobileCodeBtn", L"获取验证码")
@@ -70,7 +76,27 @@ function AccountUp.GetMobileCode( page )
     timer:Change(1000, 1000)
 
     CodePkuServiceSession:getMobileCode(
-        
+        account,
+        function ( response, err )
+            if err == 200 then
+                Mod.CodePku.MsgBox:Close()
+                if Config.defaultEnv == Config.env.RELEASE then
+                    GameLogic.AddBBS(nil, L"验证码获取成功", 3000, "255 0 0", 21)
+                else
+                    GameLogic.AddBBS(nil, L("验证码是："..response.data.code), 7000, "255 0 0", 21)
+                end
+                LOG.std(nil, "AccountUp", "GetMobileCode", "get mobile code success")
+                local mobileToken = response.data.mobile_token
+                AccountUp.mobileToken = mobileToken
+                -- MainLoginPage:SetValue('mobile_token', mobileToken)
+                return true
+            else 
+                local errMsg = response.message or "获取验证码失败"
+                -- Mod.CodePku.MsgBox:Close()
+                GameLogic.AddBBS(nil, errMsg, 3000, "255 0 0", 21)
+                return false  
+            end
+        end
     )
 end
 
@@ -96,6 +122,7 @@ function AccountUp.ShowPage()
     }
     if AccountUp.ui then
         AccountUp.ui:CloseWindow()
+        AccountUp.mobileToken = nil
     end
     AccountUp.ui = AdaptWindow:QuickWindow(params)
 end
