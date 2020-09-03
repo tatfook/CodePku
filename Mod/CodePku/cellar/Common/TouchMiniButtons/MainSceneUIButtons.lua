6bff0cbb70abb482302ca9aec3a0b0fb0ce64c0a
name <niexx@codepku.com>
2020-09-03 11:13:59 +0800

    聊天表情包

--- Mod/CodePku/cellar/Common/TouchMiniButtons/MainSceneUIButtons.lua
+++ Mod/CodePku/cellar/Common/TouchMiniButtons/MainSceneUIButtons.lua
@@ -46,6 +46,20 @@ MainSceneUIButtons.params={
     },
 }
 
+MainSceneUIButtons.Emoticons = {
+    [1]={name="sure",title="肯定",url="codepku/image/textures/chat_ui/chat_32bits.png#61 59 86 84",index =1,left=28},
+    [2]={name="shock",title="惊吓",url="codepku/image/textures/chat_ui/chat_32bits.png#175 60 86 85",index =2,left=142},
+    [3]={name="greet",title="打招呼",url="codepku/image/textures/chat_ui/chat_32bits.png#302 56 79 90",index =3,left=256},
+    [4]={name="cry",title="哭",url="codepku/image/textures/chat_ui/chat_32bits.png#422 59 78 84",index =4,left=370},
+    [5]={name="angty",title="生气",url="codepku/image/textures/chat_ui/chat_32bits.png#542 57 85 89",index =5,left=484},
+    [6]={name="smile",title="微笑",url="codepku/image/textures/chat_ui/chat_32bits.png#65 180 78 84",index =6,left=28,top=143},
+    [7]={name="vomit",title="吐",url="codepku/image/textures/chat_ui/chat_32bits.png#178 179 79 86",index =7,left=142,top=143},
+    [8]={name="dizzy",title="头晕",url="codepku/image/textures/chat_ui/chat_32bits.png#298 179 87 86)",index =8,left=256,top=143},
+    [9]={name="dislike",title="嫌弃",url="codepku/image/textures/chat_ui/chat_32bits.png#424 180 78 85",index =9,left=370,top=143},
+    [10]={name="wordless",title="无语",url="codepku/image/textures/chat_ui/chat_32bits.png#541 177 79 90",index =10,left=484,top=143},
+}
+
+
 
 MainSceneUIButtons.FriendId = nil
 
@@ -80,4 +94,13 @@ function MainSceneUIButtons.show_dialog_ui(bshow, PopIndex, friendid)
         local window = AdaptWindow:QuickWindow(params)
         return window
     end
+end
+
+function MainSceneUIButtons.show_emoticon_ui(bshow, PopIndex, friendid)
+    local param = {
+        url = "Mod/CodePku/cellar/Common/TouchMiniButtons/dialog/emoticon.html",
+        alignment="_ct", left = -960, top = -540, width = 1920, height = 1080,zorder = 30,
+    }
+    echo("-------------------show_emoticon_ui-------------------------")
+    return  AdaptWindow:QuickWindow(param)
 end
