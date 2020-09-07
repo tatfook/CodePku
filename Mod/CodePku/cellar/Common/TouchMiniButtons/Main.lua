71502820e313d60ff245eb7cafcd85296f993df0
聂晓茜 <niexx@codepku.com>
2020-09-07 10:07:40 +0800

    导航栏优化

--- Mod/CodePku/cellar/Common/TouchMiniButtons/Main.lua
+++ Mod/CodePku/cellar/Common/TouchMiniButtons/Main.lua
@@ -4,11 +4,9 @@ local AdaptWindow = commonlib.gettable("Mod.CodePku.GUI.Window.AdaptWindow")
 local EntityManager = commonlib.gettable("MyCompany.Aries.Game.EntityManager")
 local MainSceneUIButtons = commonlib.gettable("Mod.CodePku.Common.TouchMiniButtons.MainSceneUIButtons");
 
-NPL.load("(gl)script/apps/Aries/Creator/Game/GUI/TouchController.lua");
-local TouchController = commonlib.gettable("MyCompany.Aries.Game.GUI.TouchController");
-
 local MainUIButtons = NPL.export();
 
+NPL.load("(gl)Mod/CodePku/cellar/GUI/UserInfo.lua");
 MainUIButtons.hasshown = false
 
 MainUIButtons.common_window = nil
@@ -20,8 +18,44 @@ MainUIButtons.open_common = nil
 MainUIButtons.account_up = nil
 
 
+MainUIButtons.lt_buttons = {
+	[1]={url="codepku/image/textures/main_ui/main.png#41 175 105 120",top=30,width=88,height=116,name="ClickMainEntrence"},
+	[2]={url="codepku/image/textures/main_ui/main.png#165 175 90 120",top=30,width=88,height=116,name="ClickAntiJamming"},
+	[3]={url="codepku/image/textures/main_ui/main.png#38 314 108 134",top=14,width=108,height=134,name="ClickSamllMap"},
+	[4]={url="codepku/image/textures/main_ui/main.png#160 313 101 135",top=11,width=101,height=135,name="ClickCommunity"},
+	[5]={url="codepku/image/textures/main_ui/main.png#805 313 105 137",top=11,width=101,height=135,name="ClickFeedback"},
+}
+
+MainUIButtons.rb_buttons = {
+	[1]={url="codepku/image/textures/main_ui/main.png#148 25 107 142",top=22,width=103,height=134,name="ClickUserInfo"},
+	[2]={url="codepku/image/textures/main_ui/main.png#285 18 97 148",top=20,width=93,height=134,name="ClickFriend"},
+	[3]={url="codepku/image/textures/main_ui/main.png#416 18 84 148",top=20,width=80,height=134,name="ClickRank"},
+	[4]={url="codepku/image/textures/main_ui/main.png#416 18 84 148",top=20,width=100,height=134,name="ClickRank"},
+	[5]={url="codepku/image/textures/main_ui/main.png#416 18 84 148",top=20,width=80,height=134,name="ClickRank"},
+	[6]={url="codepku/image/textures/main_ui/main.png#416 18 84 148",top=20,width=80,height=134,name="ClickRank"},
+	[7]={url="codepku/image/textures/main_ui/main.png#416 18 84 148",top=20,width=80,height=134,name="ClickRank"},
+}
+
+-- get width of button group of left top corner
+function MainUIButtons.get_lt_width()
+	local width = 45;
+	for key,value in ipairs(MainUIButtons.lt_buttons) do
+		width = width + 45 + value["width"];
+	end
+	return width;
+end
+
+-- get width of button group of right bottom corner
+function MainUIButtons.get_rb_width()
+	local width = 45+82;
+	for key,value in ipairs(MainUIButtons.rb_buttons) do
+		width = width + 45 + value["width"];
+	end
+	return width;
+end
+
 function MainUIButtons.show_common_ui(flag)
-	local open_width = 804
+	local open_width = MainUIButtons.get_lt_width() + 82
 	local open_height = 178
 	local close_width = 82
 	local close_height = 178
@@ -50,11 +84,11 @@ function MainUIButtons.show_common_ui(flag)
 end
 
 function MainUIButtons.show_function_ui(flag)	--flag == true,工具栏展开
-
+	local width = MainUIButtons.get_rb_width();
 	local params = {
 		open = {
 			url="Mod/CodePku/cellar/Common/TouchMiniButtons/MainUIButtons_function.html", 
-			alignment="_rb", left = -678, top = -178, width = 678, height = 178,
+			alignment="_rb", left = -width, top = -178, width = width, height = 178,
 			click_through = true,
 		},
 		close = {
@@ -81,20 +115,9 @@ function MainUIButtons.show_dialog_ui(bshow)
 	MainUIButtons.dialog_window = MainSceneUIButtons.show_dialog_ui(bshow, 0)
 end
 
-MainUIButtons.money = {goldcoin=0, wanxuecoin=0};
 function MainUIButtons.show_money_ui()
 	local width = 746
 	local height = 89
-
-	local info = Mod.CodePku.Store:Get('user/info');
-	local wallets = info.user_wallets or {};
-	for _, v in ipairs(wallets) do
-		if v.currency_id == 1 then
-			MainUIButtons.money.goldcoin = v.amount;
-		elseif v.currency_id == 2 then
-			MainUIButtons.money.wanxuecoin = v.amount;
-		end
-	end
 
 	local params = {
 		url="Mod/CodePku/cellar/Common/TouchMiniButtons/MainMoney.html", 
@@ -116,7 +139,7 @@ function MainUIButtons.show_account_up_ui()
 	end
 end
 
-function MainUIButtons.JudgeNil()
+function MainUIButtons.ShowPage()
 	if MainUIButtons.common_window ~= nil then
 		MainUIButtons.common_window:CloseWindow()
 		MainUIButtons.common_window = nil
@@ -133,15 +156,6 @@ function MainUIButtons.JudgeNil()
 		MainUIButtons.money_window:CloseWindow()
 		MainUIButtons.money_window = nil
 	end
-	if MainUIButtons.account_up ~= nil then
-		MainUIButtons.account_up:CloseWindow()
-		MainUIButtons.account_up = nil
-	end
-end
-
-
-function MainUIButtons.ShowPage()
-	MainUIButtons.JudgeNil()
 
 	local hideMenu = false;
 	local hideAllMenu = false;
@@ -156,7 +170,7 @@ function MainUIButtons.ShowPage()
 			MainUIButtons.show_dialog_ui(false)
 			MainUIButtons.show_money_ui()		
 			MainUIButtons.show_function_ui()
-			MainUIButtons.show_account_up_ui()
+			-- MainUIButtons.show_account_up_ui()
 		else
 			MainUIButtons.show_common_ui()
 		end
