c0c20bb189156caa50716a987bbbc8571d48d892
john <867409182@qq.com>
2020-09-08 09:29:49 +0800

    update ggs

--- Mod/CodePku/online/client/GeneralGameClient.lua
+++ Mod/CodePku/online/client/GeneralGameClient.lua
@@ -1,45 +1,27 @@
---[[
-Title: OnlineClient
-Author(s): John Mai
-Date: 2020/8/2
-Desc: 客户端入口文件
-use the lib:
-------------------------------------------------------------
-NPL.load("Mod/CodePku/online/OnlineClient.lua");
-local OnlineClient = commonlib.gettable("Mod.CodePku.Online.OnlineClient");
--------------------------------------------------------
-]]
-NPL.load("Mod/GeneralGameServerMod/Core/Common/Connection.lua");
-NPL.load("Mod/GeneralGameServerMod/Core/Common/Log.lua");
-NPL.load("Mod/GeneralGameServerMod/Core/Common/Common.lua");
-NPL.load("Mod/CodePku/online/EntityOtherPlayer.lua");
-NPL.load("Mod/CodePku/online/EntityMainPlayer.lua");
 NPL.load("Mod/GeneralGameServerMod/Core/Client/GeneralGameClient.lua");
+NPL.load("Mod/CodePku/online/client/EntityOtherPlayer.lua");
+NPL.load("Mod/GeneralGameServerMod/Core/Common/Log.lua");
 
-local OnlineClient = commonlib.inherit(commonlib.gettable("Mod.GeneralGameServerMod.Core.Client.GeneralGameClient"), commonlib.gettable("Mod.CodePku.Online.OnlineClient"));
-local EntityMainPlayer = commonlib.gettable("Mod.CodePku.Online.EntityMainPlayer");
-local EntityOtherPlayer = commonlib.gettable("Mod.CodePku.Online.EntityOtherPlayer");
-local Common = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Common");
+local EntityOtherPlayer = commonlib.gettable("Mod.CodePku.Online.Client.EntityOtherPlayer");
+local GeneralGameClient = commonlib.inherit(commonlib.gettable("Mod.GeneralGameServerMod.Core.Client.GeneralGameClient"), commonlib.gettable("Mod.CodePku.Online.Client.GeneralGameClient"));
+local Config = NPL.load("(gl)Mod/CodePku/online/client/Config.lua");
 local Log = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Log");
-local Connection = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Connection");
 
-local Config = NPL.load("(gl)Mod/CodePku/online/Config.lua");
+function GeneralGameClient:ctor()
+    self.userinfo = System.User;
 
-local Packets = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Packets");
-
-local moduleName = "Mod.CodePku.Online.OnlineClient";
-
--- 构造函数
-function OnlineClient:ctor()
-
+    GameLogic.GetFilters():add_filter("ggs", function(msg)
+        if (type(msg) == "table" and msg.action == "UpdateNickName") then
+            self.userinfo.nickname = msg.nickname;
+        end
+        return msg;
+    end);
 end
 
--- 初始化函数
-function OnlineClient:Init()
+function GeneralGameClient:Init()
     if (self.inited) then return end
 
-    -- 基类初始化
-    OnlineClient._super.Init(self);
+    GeneralGameClient._super.Init(self);
 
     self.GetAssetsWhiteList().AddAsset("codepku/model/HLS_AN.x");
     self.GetAssetsWhiteList().AddAsset("codepku/model/LLS_AN.x");
@@ -48,40 +30,10 @@ function OnlineClient:Init()
     self:GetOptions().serverIp = Config.defaultOnlineServer.host;
     self:GetOptions().serverPort = Config.defaultOnlineServer.port;
 
-    -- self:GetOptions().ip = Config.defaultOnlineServer.host;
-    -- self:GetOptions().port = Config.defaultOnlineServer.port;
     self.inited = true;
 end
 
--- 获取世界类
-function OnlineClient:GetGeneralGameWorldClass()
-    return OnlineClient._super.GetGeneralGameWorldClass(self);  -- 不定制
-end
--- 获取网络处理类
-function OnlineClient:GetNetClientHandlerClass()
-    return OnlineClient._super.GetNetClientHandlerClass(self);  -- 不定制
-end
-
--- 获取主玩家类
-function OnlineClient:GetEntityMainPlayerClass()
-    return EntityMainPlayer;
-end
--- 获取其它玩家类
-function OnlineClient:GetEntityOtherPlayerClass()
-    return EntityOtherPlayer;
-end
-
--- 获取当前认证用户信息
-function OnlineClient:GetUserInfo()
-    return System.User;
-end
-
--- 是否是匿名用户 移除匿名功能
-function OnlineClient:IsAnonymousUser()    
-    return false;
-end
-
-function OnlineClient:LoadWorld(opts)
+function GeneralGameClient:LoadWorld(opts)
     -- 初始化
     self:Init();
     local worldUrl = opts.worldUrl;
@@ -94,9 +46,6 @@ function OnlineClient:LoadWorld(opts)
     -- 确定世界ID
     options.worldId = tostring(options.worldId or curWorldId or Config.defaultOnlineServer.defaultWorldId);
     options.username = options.username or self:GetUserInfo().username;
-
-    -- 打印选项值
-    Log:Info(options);
 
     -- only reload world if world id does not match
     local isReloadWorld = tostring(options.worldId) ~= tostring(curWorldId); 
@@ -158,7 +107,29 @@ function OnlineClient:LoadWorld(opts)
     else
         self:OnWorldLoaded();
     end
+
+    local options = self:GetOptions();
+    self.userinfo.nickname = options.nickname or self.userinfo.nickname;
+end
+
+-- 获取其它玩家类
+function GeneralGameClient:GetEntityOtherPlayerClass()
+    return EntityOtherPlayer;
+end
+
+-- 获取当前认证用户信息
+-- 此函函数返回用户信息会在各玩家间同步, 所以尽量精简
+function GeneralGameClient:GetUserInfo()
+    return self.userinfo;
+end
+
+-- 是否是匿名用户
+function GeneralGameClient:IsAnonymousUser()
+    local isAnonymousUser = self:GetOptions().isAnonymousUser;
+    if (isAnonymousUser ~= nil) then return isAnonymousUser end
+
+    return System.User.isVisitor;
 end
 
 -- 初始化成单列模式
-OnlineClient:InitSingleton();
+GeneralGameClient:InitSingleton();
--- Mod/CodePku/online/main.lua
+++ Mod/CodePku/online/main.lua
@@ -11,29 +11,64 @@ Online:init();
 -------------------------------------------------------
 ]]
 
+-- NPL.load("Mod/GeneralGameServerMod/Core/Common/Log.lua");
+-- local Online = commonlib.gettable("Mod.CodePku.Online");
+-- local Commands = commonlib.gettable("MyCompany.Aries.Game.Commands");
+-- local Config = NPL.load("./Config.lua");
+-- local CmdParser = commonlib.gettable("MyCompany.Aries.Game.CmdParser");	
+-- local Log = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Log");
+-- local GeneralGameServerMod = commonlib.gettable("Mod.GeneralGameServerMod");
+
+-- NPL.load("Mod/GeneralGameServerMod/App/Client/AppGeneralGameClient.lua");
+-- local AppGeneralGameClient = commonlib.gettable("Mod.GeneralGameServerMod.App.Client.AppGeneralGameClient");
+
+-- function Online:Init()
+-- 	NPL.load("(gl)Mod/GeneralGameServerMod/main.lua");                                    -- 此行可省略 GGS是内置模块, 程序启动会自动加载
+-- 	local GeneralGameServerMod = commonlib.gettable("Mod.GeneralGameServerMod");      -- 获取GGS模块
+
+-- 	NPL.load("(gl)Mod/CodePku/online/OnlineClient.lua");
+-- 	local OnlineClient = commonlib.gettable("Mod.CodePku.Online.OnlineClient");
+-- 	GeneralGameServerMod:RegisterClientClass("CodePku", OnlineClient);
+
+-- 	self:RegisterCommand();
+-- end
+
+
+-- function Online:RegisterCommand()
+--     Commands["connectCodePku"] = {
+-- 		mode_deny = "",  
+-- 		name="connectCodePku",
+-- 		quick_ref="/connectCodePku [worldId] [parallelWorldName]", 
+-- 		desc=[[进入联机玩学世界 
+-- worldId 为世界ID(未指定或为0则联机当前世界或默认世界)
+-- parallelWorldName 平行世界名, 可选. 指定世界的副本世界
+-- 示例:
+-- connectCodePku                        # 联机进入当前世界或默认世界
+-- connectCodePku 145                    # 联机进入世界ID为145的世界
+-- connectCodePku 145 parallel           # 联机进入世界ID为145的平行世界 parallel
+-- ]], 
+-- 		handler = function(cmd_name, cmd_text, cmd_params, fromEntity)		
+--             Log:Info("run cmd: %s %s", cmd_name, cmd_text);
+--             local ggsCmd = string.format("/ggs connect -app=CodePku %s", cmd_text);
+--             Log:Info("run ggs connect cmd: %s", ggsCmd);
+--             GameLogic.RunCommand(string.format("/ggs connect -app=CodePku %s", cmd_text));
+-- 		end,
+-- 	}
+-- end
+NPL.load("(gl)Mod/GeneralGameServerMod/main.lua"); 
+NPL.load("(gl)Mod/CodePku/online/client/GeneralGameClient.lua");
 NPL.load("Mod/GeneralGameServerMod/Core/Common/Log.lua");
+
+local GeneralGameServerMod = commonlib.gettable("Mod.GeneralGameServerMod");
+local GeneralGameClient = commonlib.gettable("Mod.CodePku.Online.Client.GeneralGameClient");
 local Online = commonlib.gettable("Mod.CodePku.Online");
 local Commands = commonlib.gettable("MyCompany.Aries.Game.Commands");
-local Config = NPL.load("./Config.lua");
-local CmdParser = commonlib.gettable("MyCompany.Aries.Game.CmdParser");	
 local Log = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Log");
-local GeneralGameServerMod = commonlib.gettable("Mod.GeneralGameServerMod");
-
-NPL.load("Mod/GeneralGameServerMod/App/Client/AppGeneralGameClient.lua");
-local AppGeneralGameClient = commonlib.gettable("Mod.GeneralGameServerMod.App.Client.AppGeneralGameClient");
 
 function Online:Init()
-	NPL.load("(gl)Mod/GeneralGameServerMod/main.lua");                                    -- 此行可省略 GGS是内置模块, 程序启动会自动加载
-	local GeneralGameServerMod = commonlib.gettable("Mod.GeneralGameServerMod");      -- 获取GGS模块
-
-	NPL.load("(gl)Mod/CodePku/online/OnlineClient.lua");
-	local OnlineClient = commonlib.gettable("Mod.CodePku.Online.OnlineClient");
-	GeneralGameServerMod:RegisterClientClass("CodePku", OnlineClient);
-
+	GeneralGameServerMod:RegisterClientClass("CodePku", GeneralGameClient);
 	self:RegisterCommand();
 end
-
-
 function Online:RegisterCommand()
     Commands["connectCodePku"] = {
 		mode_deny = "",  
