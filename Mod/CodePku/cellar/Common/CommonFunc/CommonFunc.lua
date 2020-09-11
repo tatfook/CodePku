130b139c5153ccea6fffaef0322f71e3324310c7
聂晓茜 <niexx@codepku.com>
2020-09-11 10:02:26 +0800

    IOS渠道审核

--- Mod/CodePku/cellar/Common/CommonFunc/CommonFunc.lua
+++ Mod/CodePku/cellar/Common/CommonFunc/CommonFunc.lua
@@ -4,6 +4,9 @@ Author(s): NieXX
 Date: 2020/9/10
 Desc:
 use the lib:
+virtual functions:
+    isHuaweiApproval()
+    isIOSApproval()
 ------------------------------------------------------------
 NPL.load("(gl)Mod/CodePku/cellar/Common/CommonFunc/CommonFunc.lua")
 local CommonFunc = commonlib.gettable("Mod.CodePku.Common.CommonFunc")
@@ -15,6 +18,10 @@ NPL.load("(gl)script/ide/System/os/os.lua");
 
 -- local CommonFunc = NPL.export();
 
+
+-- Judge whether to pass the channel audit, such as huawei, flyme, sogou
+-- e.g. CommonFunc.isHuaweiApproval();
+-- @return:if successful return true, if falied return false
 CommonFunc.isHuaweiApproval = function ()
     local huaweiApprovalStatus = Mod.CodePku.BasicConfigTable.huawei_approval_status == 'on'
     local app_market = ParaEngine.GetAppCommandLineByParam("app_market", "")
@@ -29,15 +36,12 @@ CommonFunc.isHuaweiApproval = function ()
     end
 end
 
+-- Judge whether IOS channel is approved or not
+-- e.g. CommonFunc.isIOSApproval();
+-- @return:if successful return true, if falied return false
 CommonFunc.isIOSApproval = function ()
-    -- local iosApprovalStatus = Mod.CodePku.BasicConfigTable.ios_approval_status == 'on'
+    local iosApprovalStatus = Mod.CodePku.BasicConfigTable.ios_approval_status == 'on'
     local mock_ios = ParaEngine.GetAppCommandLineByParam("mock_ios", "") == "true"
     local isIOSPlatform = System.os.GetPlatform() == 'ios'
-    local iosApprovalStatus = true
-    -- echo("---------isIOSApproval---------")
-    -- echo(System.os.GetPlatform())
-    -- echo(isIOSPlatform)
-    -- echo(mock_ios)
-    -- echo((isIOSPlatform or mock_ios) and iosApprovalStatus)
     return (isIOSPlatform or mock_ios) and iosApprovalStatus
 end
