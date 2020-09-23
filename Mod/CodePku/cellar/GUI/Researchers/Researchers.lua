5e7c06bdce19e6193a88aa2a34e6101d2c98c5c2
loujy <loujy@codepku.com>
2020-09-23 17:37:43 +0800

    暂存

--- Mod/CodePku/cellar/GUI/Researchers/Researchers.lua
+++ Mod/CodePku/cellar/GUI/Researchers/Researchers.lua
@@ -12,3 +12,13 @@ Researchers:ShowPage(id);
 local Researchers = NPL.export();
 local AdaptWindow = commonlib.gettable("Mod.CodePku.GUI.Window.AdaptWindow")
 local request = NPL.load("(gl)Mod/CodePku/api/BaseRequest.lua");
+
+
+Researchers.params = {
+    [1] = {url="Mod/CodePku/cellar/GUI/Researchers/Board.html", alignment="_lt", left = 0, top = 0, width = 1920 , height = 1080, zorder = 30,},
+}
+
+function Researchers:ShowPage(id)
+    local id = tonumber(id)
+    
+end
