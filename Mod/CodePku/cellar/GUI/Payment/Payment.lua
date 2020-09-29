98d38e30c4c206a7ffdaeddc64dd64a6f80986e8
loujy <loujy@codepku.com>
2020-09-29 19:50:17 +0800

    添加充值通知

--- Mod/CodePku/cellar/GUI/Payment/Payment.lua
+++ Mod/CodePku/cellar/GUI/Payment/Payment.lua
@@ -17,6 +17,7 @@ local request = NPL.load("(gl)Mod/CodePku/api/BaseRequest.lua");
 local UserInfoPage = NPL.load("(gl)Mod/CodePku/cellar/GUI/UserInfo.lua");
 
 Payment.iconPng = "codepku/image/textures/common_32bits.png"
+Payment.Recharge_send_content = "已推送课程解锁页面到您家长微信~快和爸爸妈妈沟通购买课程吧。"
 
 Payment.params = {
     -- 解锁提示页面
@@ -27,7 +28,7 @@ Payment.params = {
     -- 充值提示页面
     Recharge = {
         url="Mod/CodePku/cellar/GUI/Payment/Recharge.html", 
-        alignment="_lt", left = 0, top = 0, width = 1920 , height = 1080, zorder = 32
+        alignment="_lt", left = 0, top = 0, width = 1920 , height = 1080, zorder = 33
       },
     -- 家长绑定页面
     Eldership = {
@@ -68,14 +69,39 @@ function Payment:ShowPage(pagename)
     AdaptWindow:QuickWindow(Payment.params[ToPage])
 end
 
+
 function Payment:PurchaseNotice()
+    if (not Payment.TimerTimes) or (Payment.TimerTimes < 1) then
+        if Payment.isClickedPurchaseNotice then
+            return
+        end
+        Payment:SendNotice()
+        Payment.isClickedPurchaseNotice = true
+        Payment.TimerTimes = 5
+        Payment.Recharge_send_content = "课程解锁申请已推送到您家长微信啦~请勿频繁点击哦"
+        local timer = commonlib.Timer:new({
+            callbackFunc = function(timer)
+                if Payment.TimerTimes == 0 then
+                    Payment.isClickedPurchaseNotice = false
+                    Payment.Recharge_send_content = "已推送课程解锁页面到您家长微信~快和爸爸妈妈沟通购买课程吧。"
+                    Payment:SendNotice()
+                    timer:Change(nil, nil)
+                end
+                Payment.TimerTimes = Payment.TimerTimes - 1
+            end
+        })
+        timer:Change(1000, 1000)
+    end
+end
+
+function Payment:SendNotice()
     -- todo 发送购买通知
     local params = {
         entity_id = Payment.entity_id,
         entity_type = Payment.entity_type,
     }
-    request:post('',params):next(function(response)
-        
+    request:post('/notices/parent-unlock-course',params):next(function(response)
+
     end):catch(function(e)
         GameLogic.AddBBS("CodeGlobals", e.data.message, 3000, "#FF0000");
     end);
