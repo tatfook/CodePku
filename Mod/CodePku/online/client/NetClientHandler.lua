b64c1b93fb4138f18044167f8df46694ad9831e5
zouren <277714787@qq.com>
2020-11-03 16:27:57 +0800

    登陆的时候给ggs的options里s里边多传一个no字段

--- Mod/CodePku/online/client/NetClientHandler.lua
+++ Mod/CodePku/online/client/NetClientHandler.lua
@@ -11,7 +11,7 @@ local NetClientHandler = commonlib.gettable("Mod.CodePku.Online.Client.NetClient
 NPL.load("Mod/GeneralGameServerMod/Core/Client/NetClientHandler.lua");
 
 local NetClientHandler = commonlib.inherit(commonlib.gettable("Mod.GeneralGameServerMod.Core.Client.NetClientHandler"), commonlib.gettable("Mod.CodePku.Online.Client.NetClientHandler"));
-
+local Packets = commonlib.gettable("Mod.GeneralGameServerMod.Core.Common.Packets")
 
 function NetClientHandler:ctor() 
 
@@ -24,7 +24,6 @@ function NetClientHandler:Init(world)
 	return self;
 end
 
--- 处理方块点击
 function NetClientHandler:handleGeneral(packetGeneral)
     local action = packetGeneral.action;
     if (action == "CodePKU") then 
@@ -35,4 +34,23 @@ function NetClientHandler:handleGeneral(packetGeneral)
     else
         self._super.handleGeneral(self, packetGeneral)
     end
+end
+
+-- 登录
+function NetClientHandler:Login()
+    local options = self:GetClient():GetOptions();
+    self:AddToSendQueue(Packets.PacketPlayerLogin:new():Init({
+        username = options.username,
+        password = options.password,
+        worldId = options.worldId,
+        worldName = options.worldName,
+        worldType = options.worldType,
+        options = {
+            isSyncBlock = options.isSyncBlock,
+            isSyncForceBlock = options.isSyncForceBlock,
+            isSyncCmd = options.isSyncCmd,
+            areaSize = options.areaSize,
+            no = options.no or "1"
+        }
+    }));
 end
