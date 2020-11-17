ebea1db012160efa34045c0656fd677a06e6621e
wanglj <wanglj@codepku.com>
2020-11-17 15:19:43 +0800

    暂存

--- Mod/CodePku/cellar/GUI/InviteCode/InviteCode.lua
+++ Mod/CodePku/cellar/GUI/InviteCode/InviteCode.lua
@@ -4,20 +4,21 @@ local InviteCode = NPL.load("(gl)Mod/CodePku/cellar/GUI/InviteCode/InviteCode.lu
 local AdaptWindow = commonlib.gettable("Mod.CodePku.GUI.Window.AdaptWindow");
 local InviteCode = NPL.export();
 
---local inviteCodeImageData = NPL.load("(gl)Mod/CodePku/cellar/imageLuaTable/inviteCodeImageData.lua")
+local inviteImageData = NPL.load("(gl)Mod/CodePku/cellar/imageLuaTable/inviteImageData.lua")
 local mainFrameImageData = NPL.load("(gl)Mod/CodePku/cellar/imageLuaTable/mainFrameImageData.lua")
 
 function InviteCode.GetIconUrl(iconName)
-    if iconName == 'main_icon_coin_1.png' or iconName == 'main_icon_coin_2.png' then
+    if iconName == 'main_icon_coin_1.png' or iconName == 'main_icon_coin_2.png' or iconName == 'main_friends_call.png' then
         return mainFrameImageData:GetIconUrl(iconName)
     else
-        return ''
+        echo(string.format("InviteCode.GetIconUrl: %s",inviteImageData:GetIconUrl(iconName)))
+        return inviteImageData:GetIconUrl(iconName)
     end
 end
 
 function InviteCode:ShowPage()
     local params = {
-        url = "Mod/CodePku/cellar/GUI/InviteCode/InviteCodeMain.html",
+        url = "Mod/CodePku/cellar/GUI/InviteCode/InviteCodePage.html",
         alignment = "_ct",
         x = -960,
         y = -540,
