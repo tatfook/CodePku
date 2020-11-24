a83d8bc7f6705bd6361ef5ee8882a14e67c134a6
zouren <277714787@qq.com>
2020-11-24 15:16:57 +0800

    stash

---
+++ Mod/CodePku/cellar/imageLuaTable/funcKeyboardImageData.lua
@@ -0,0 +1,32 @@
+--[[
+des: funcKeyboard image data file
+local funcKeyboardImageData = NPL.load("(gl)Mod/CodePku/cellar/imageLuaTable/funcKeyboardImageData.lua")
+]]
+
+local funcKeyboardImageData = NPL.export()
+
+local imageTable = {
+	["frames"] = {
+		["funckeyboard_bot.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 535, ["top"] = 958, ["width"] = 429, ["height"] = 577,},
+		["funckeyboard_bot02.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 2, ["top"] = 2, ["width"] = 749, ["height"] = 953,},
+		["funckeyboard_bot_grid.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 2, ["top"] = 958, ["width"] = 530, ["height"] = 650,},
+		["funckeyboard_key_bot.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 754, ["top"] = 2, ["width"] = 192, ["height"] = 80,},
+		["funckeyboard_key_bot_01.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 754, ["top"] = 85, ["width"] = 192, ["height"] = 80,},
+		["funckeyboard_key_dark.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 754, ["top"] = 168, ["width"] = 190, ["height"] = 78,},
+		["funckeyboard_keypad_titlemat.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 754, ["top"] = 249, ["width"] = 166, ["height"] = 41,},
+		["funckeyboard_tips_question.png"] = { ["url"] = "codepku/image/textures/UI/funcKeyboard/funcKeyboard.png", ["left"] = 949, ["top"] = 2, ["width"] = 34, ["height"] = 33,},
+	},
+}
+
+function funcKeyboardImageData:GetIconUrl(index)
+	local path = ""
+	path = path..imageTable["frames"][index].url
+	if imageTable["frames"][index].left then
+		path = path..'#'
+		path = path..tostring(imageTable["frames"][index].left)
+		path = path..' '..tostring(imageTable["frames"][index].top)
+		path = path..' '..tostring(imageTable["frames"][index].width)
+		path = path..' '..tostring(imageTable["frames"][index].height)
+	end
+	return path
+end
