03d59298b05c57db33b403cafb1494756de1d528
loujy <loujy@codepku.com>
2020-11-26 11:28:05 +0800

    暂存

---
+++ Mod/CodePku/cellar/GUI/LiveLesson/Entrance/LiveLessonEntrance.lua
@@ -0,0 +1,10 @@
+--[[
+Title: LiveLessonEntrance
+Author: loujiayu
+Date: 2020/11/26
+
+Example:
+-----------------------------------------------
+local LiveLessonEntrance = NPL.load("(gl)Mod/CodePku/cellar/GUI/LiveLesson/Entrance/LiveLessonEntrance.lua");
+-----------------------------------------------
+]]
