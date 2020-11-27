939c9499767e65fe35ae0d0ef376a9e333fa25ac
loujy <loujy@codepku.com>
2020-11-27 10:45:14 +0800

    沉浸式课堂入口框架

--- Mod/CodePku/cellar/GUI/LiveLesson/Entrance/LiveLessonEntrance.lua
+++ Mod/CodePku/cellar/GUI/LiveLesson/Entrance/LiveLessonEntrance.lua
@@ -11,6 +11,93 @@ local LiveLessonEntrance = NPL.load("(gl)Mod/CodePku/cellar/GUI/LiveLesson/Entra
 
 local LiveLessonEntrance = NPL.export();
 local AdaptWindow = commonlib.gettable("Mod.CodePku.GUI.Window.AdaptWindow")
-local live_lessonImageData = NPL.load("(gl)Mod/CodePku/cellar/imageLuaTable/live_lessonImageData.lua")
+local liveLessonImageData = NPL.load("(gl)Mod/CodePku/cellar/imageLuaTable/liveLessonImageData.lua")
 local request = NPL.load("(gl)Mod/CodePku/api/BaseRequest.lua");
 
+-- 常量table
+LiveLessonEntrance.constant = {
+    MatchCodeLimit = 6,     -- 匹配码输入字数限制
+    RoomNameLimit = 30,     -- 房间名字数限制
+    CoursewareIDLimit = 8   -- 课件ID字数限制
+}
+
+-- 获取图标
+function LiveLessonEntrance:GetIconPath(index)
+    return liveLessonImageData:GetIconUrl(index)
+end
+
+-- 加入页面有输入框，需要把两个窗口都关掉
+function LiveLessonEntrance:EntrancePageSpecialClose()
+    if LiveLessonEntrance.EmptyBG then
+        LiveLessonEntrance.EmptyBG:CloseWindow()
+        LiveLessonEntrance.EmptyBG = nil
+    end
+    if LiveLessonEntrance.frist_page then
+        LiveLessonEntrance.frist_page:CloseWindow()
+        LiveLessonEntrance.frist_page = nil
+    end
+end
+
+-- 加入房间弹窗
+function LiveLessonEntrance:ShowEntrancePage()
+    -- 这里有输入框，为了IOS适配，需要特殊处理下，再开一个空页面放下面
+    if LiveLessonEntrance.EmptyBG then
+        LiveLessonEntrance.EmptyBG:CloseWindow()
+        LiveLessonEntrance.EmptyBG = nil
+    end
+
+    local BGparams = {
+        url="Mod/CodePku/cellar/GUI/LiveLesson/LiveLessonEmptyPage.html",
+        alignment="_lt", left = 0, top = 0, width = 1920 , height = 1080, zorder = 21,
+    }
+    LiveLessonEntrance.EmptyBG = AdaptWindow:QuickWindow(BGparams)
+
+    -- 加入房间页面
+    if LiveLessonEntrance.frist_page then
+        LiveLessonEntrance.frist_page:CloseWindow()
+        LiveLessonEntrance.frist_page = nil
+    end
+    local params = {
+        url="Mod/CodePku/cellar/GUI/LiveLesson/Entrance/LiveLessonEntrance.html",
+        alignment="_lt", left = 608, top = 200, width = 704 , height = 528, zorder = 22
+    }
+    LiveLessonEntrance.frist_page = AdaptWindow:QuickWindow(params)
+    return LiveLessonEntrance.frist_page
+end
+
+function LiveLessonEntrance:EstablishPageSpecialClose()
+    if LiveLessonEntrance.EmptyBG then
+        LiveLessonEntrance.EmptyBG:CloseWindow()
+        LiveLessonEntrance.EmptyBG = nil
+    end
+    if LiveLessonEntrance.establish_page then
+        LiveLessonEntrance.establish_page:CloseWindow()
+        LiveLessonEntrance.establish_page = nil
+    end
+end
+
+-- 创建房间弹窗
+function LiveLessonEntrance:ShowEstablishPage()
+    if LiveLessonEntrance.EmptyBG then
+        LiveLessonEntrance.EmptyBG:CloseWindow()
+        LiveLessonEntrance.EmptyBG = nil
+    end
+
+    local BGparams = {
+        url="Mod/CodePku/cellar/GUI/LiveLesson/LiveLessonEmptyPage.html",
+        alignment="_lt", left = 0, top = 0, width = 1920 , height = 1080, zorder = 21,
+    }
+    LiveLessonEntrance.EmptyBG = AdaptWindow:QuickWindow(BGparams)
+
+    -- 创建房间页面
+    if LiveLessonEntrance.establish_page then
+        LiveLessonEntrance.establish_page:CloseWindow()
+        LiveLessonEntrance.establish_page = nil
+    end
+    local params = {
+        url="Mod/CodePku/cellar/GUI/LiveLesson/Entrance/LiveLessonEstablish.html",
+        alignment="_lt", left = 608, top = 200, width = 704 , height = 528, zorder = 22
+    }
+    LiveLessonEntrance.establish_page = AdaptWindow:QuickWindow(params)
+    return LiveLessonEntrance.establish_page
+end
--- Mod/CodePku/cellar/imageLuaTable/liveLessonImageData.lua
+++ Mod/CodePku/cellar/imageLuaTable/liveLessonImageData.lua
@@ -7,15 +7,20 @@ local liveLessonImageData = NPL.export()
 
 local imageTable = {
 	["frames"] = {
+		["live_lesson_btn_blue_big.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 287, ["width"] = 199, ["height"] = 67,},
+		["live_lesson_btn_yellow_big.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 111, ["width"] = 320, ["height"] = 105,},
+		["live_lesson_chuangjian.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 2, ["top"] = 539, ["width"] = 158, ["height"] = 39,},
 		["live_lesson_cion01.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 2, ["width"] = 337, ["height"] = 106,},
-		["live_lesson_cion_pin.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 872, ["top"] = 179, ["width"] = 103, ["height"] = 113,},
-		["live_lesson_cion_return01.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 179, ["width"] = 198, ["height"] = 67,},
-		["live_lesson_cion_return02.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 872, ["top"] = 295, ["width"] = 96, ["height"] = 37,},
+		["live_lesson_cion_pin.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 873, ["top"] = 287, ["width"] = 103, ["height"] = 113,},
+		["live_lesson_cion_return01.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 357, ["width"] = 198, ["height"] = 67,},
+		["live_lesson_cion_return02.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 872, ["top"] = 403, ["width"] = 97, ["height"] = 39,},
 		["live_lesson_comm.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 2, ["top"] = 2, ["width"] = 666, ["height"] = 470,},
-		["live_lesson_comm_frame.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 111, ["width"] = 316, ["height"] = 65,},
-		["live_lesson_frame_generatebtn.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 833, ["top"] = 335, ["width"] = 86, ["height"] = 43,},
-		["live_lesson_icon_enter_into01.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 249, ["width"] = 198, ["height"] = 67,},
-		["live_lesson_icon_enter_into02.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 319, ["width"] = 159, ["height"] = 39,},
+		["live_lesson_comm_frame.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 219, ["width"] = 316, ["height"] = 65,},
+		["live_lesson_comm_x.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 872, ["top"] = 491, ["width"] = 77, ["height"] = 123,},
+		["live_lesson_frame_generatebtn.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 872, ["top"] = 445, ["width"] = 86, ["height"] = 43,},
+		["live_lesson_icon_enter_into01.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 671, ["top"] = 427, ["width"] = 198, ["height"] = 67,},
+		["live_lesson_icon_enter_into02.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 2, ["top"] = 617, ["width"] = 159, ["height"] = 39,},
+		["live_lesson_icon_enter_into03.png"] = { ["url"] = "codepku/image/textures/UI/liveLesson/liveLesson.png", ["left"] = 2, ["top"] = 497, ["width"] = 159, ["height"] = 39,},
 	},
 }
 
